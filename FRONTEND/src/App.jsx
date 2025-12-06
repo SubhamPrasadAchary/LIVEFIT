83bd4f97a8b1afd77ff2e375b66d24b799fed2e6
Subham Prasad Achary <acharysubham2803@gmail.com>
2025-12-06 23:19:14 +0530

    Update App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ParticlesBackground from './components/ParticlesBackground';
 import './App.css';
 
 function App() {
+  // This effect will run once when the component mounts
+  useEffect(() => {
+    // Move the existing content into the root div
+    const content = document.querySelector('body').innerHTML;
+    const root = document.getElementById('root');
+    if (root) {
+      root.innerHTML = content;
+    }
+  }, []);
+
   return (
     <div className="app">
       <ParticlesBackground />
       <div className="content">
-        {/* Your existing HTML content will be rendered here */}
-        <div id="root"></div>
+        {/* The existing content will be moved here by the useEffect hook */}
       </div>
     </div>
   );
